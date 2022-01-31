6c0a8ec6507aa94d1bf9546a3ed21e4c22077719
antoshka <ashebalkin@diasoft.ru>
2022-01-31 22:19:12 +0300

    Initial commit

---
+++ src/main/java/ru/ashebalkin/skypro/course2/lesson6/examcoursework/ExamcourseworkApplication.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.lesson6.examcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamcourseworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamcourseworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ashebalkin/skypro/course2/lesson6/examcoursework/ExamcourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.lesson6.examcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamcourseworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
